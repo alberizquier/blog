d4c0f5fa09ff52cac0f0a154e5fbe5a65f8ab4d2
Alberto Izquierdo <alberto.izquierdo@kairosds.com>
2020-01-24 10:39:32 +0100

    Middleware comprobación de palabras

--- back/middlewares/wordVerify.js
+++ back/middlewares/wordVerify.js
@@ -1,15 +1,18 @@
-let wordVerify = (req, res, next) => {
-    // let role = req.user.role;
-    // if (role === 'publisher' || role === 'admin') {
-    //     next();
-    // } else {
-    //     return res.json({
-    //         ok: false,
-    //         err: {
-    //             msg: `User doesn't have permission`
-    //         }
-    //     });
-    // }
+
+let wordVerify = (sentence, words) => {
+    let result = [];
+    for (let i = 0; i < words.length; i++) {
+        if (sentence.includes(words[i])) {
+            result.push(words[i]);
+        }
+    }
+    if (result.length === 0) {
+        console.log('Your sentence has been published');
+    } else {
+        console.log(result);
+    }
 };
+
+wordVerify("This is an example", ['test', 'example', 'try', 'one']);
 
 module.exports = wordVerify;
